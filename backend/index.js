e359e3cc34ef68a8f5046c2ded9b5c221966d58a
5arSanti <santiari05@hotmail.com>
2023-10-18 14:16:18 -0500

    Small implementation with expectation of a correct database connection

--- backend/index.js
+++ backend/index.js
@@ -36,11 +36,39 @@ const dbConfig = {
 // Conectar a la base de datos
 sql.connect(dbConfig, (err) => {
 	if (err) {
-	console.error('Error al conectar a la base de datos:', err);
+		console.error('Error al conectar a la base de datos:', err);
 	} else {
-	console.log('Conexión a la base de datos exitosa');
+		console.log('Conexión a la base de datos exitosa');
 	}
 });
+
+
+const sqlConfig = {
+	user: "",
+	password: "",
+	database: "",
+	server: "",
+	pool: {
+		max: 10,
+		min: 0,
+		idleTimeoutMillis: 30000
+	},
+	options: {
+		trustServerCertificate: true
+	}
+}
+
+async () => {
+	try {
+		await sql.connect(sqlConfig)
+		const result = await sql.query("select * from mytable");
+		console.dir(result)
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+
 //---------------------------------------
 
 app.get("/", (request, response) => {
