6eded9ca64d1ddaee8c7cd2f7343bf0bcb813003
5arSanti <santiari05@hotmail.com>
2023-11-14 12:52:50 -0500

    Transitions made

--- frontend/src/Pages/components/MunicipalityGrid/MunicipalityCard/index.jsx
+++ frontend/src/Pages/components/MunicipalityGrid/MunicipalityCard/index.jsx
@@ -1,15 +1,10 @@
-import React from "react";
 import "./styles.css";
 
 const MunicipalityCard = ({text, value, onClick}) => {
-    const [isCLicked, setIsClicked] = React.useState(null);
-
     return(
-        <div className={`municipality-card-container ${isCLicked ? "municipality-card-container-clicked" : ""}`} 
+        <div className={`municipality-card-container`} 
             onClick={() => {
                 onClick(text)
-                setIsClicked(false);
-                setIsClicked(true);
             }}
         >
             <p>{text}</p>
