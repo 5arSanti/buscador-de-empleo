be14c40739bd90b57dc05ce1e95b08ccce1a7ae8
Pablocarr <pablocarrh@gmail.com>
2024-01-25 08:59:49 -0500

    Icons

--- frontend/src/Context/index.jsx
+++ frontend/src/Context/index.jsx
@@ -10,8 +10,8 @@ const AppProvider = ({children}) => {
     }
 
     //API -- Cambiar el valor de la variable api segun la infraestructura de produccion
-    const domain = "http://localhost:3080";
-	const api = `${domain}/api/v1`;
+    const domain = "http://localhost:15105";
+	const api = `${domain}/bue/api/v1`;
 
 	//-------------------------------------
     const [apiUri, setApiUri] = React.useState(api);
